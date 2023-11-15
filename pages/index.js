95ae195365eabafcbcdb309ea3e8ceb2decc8a65
jlevigt <150632922+jlevigt@users.noreply.github.com>
2023-11-15 16:19:23 +0000

    adiciona link de castelo triste

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Vai ouvir CASTELO TRISTE</h1>
+    return <h1>Vai ouvir <a href="https://music.youtube.com/watch?v=z_VnfSWLBbk&si=UzYO88adeJdNQ4A1" target="_blank" rel="external">CASTELO TRISTE</a></h1>
 }
 
 export default Home;
