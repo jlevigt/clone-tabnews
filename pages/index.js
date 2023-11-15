21cb0b4be6249fcce38bf56a2448b8922bf4ada4
jlevigt <150632922+jlevigt@users.noreply.github.com>
2023-11-15 18:36:38 +0000

    estilização ChatGPT

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,43 @@
+// index.js
+
+import React from 'react';
+
 function Home() {
-    return <h1>Vai ouvir <a href="https://music.youtube.com/watch?v=z_VnfSWLBbk&si=UzYO88adeJdNQ4A1" target="_blank" rel="external">CASTELO TRISTE</a></h1>
+    return (
+        <div className="container">
+            <h1>Vai ouvir <a href="https://music.youtube.com/watch?v=z_VnfSWLBbk&si=UzYO88adeJdNQ4A1" target="_blank" rel="external">CASTELO TRISTE</a></h1>
+            <style jsx global>{`
+                body {
+                    margin: 0;
+                    padding: 0;
+                }
+
+                .container {
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    min-height: 100vh;
+                    background-color: black;
+                    color: white;
+                    font-family: 'Arial', sans-serif;
+                    font-weight: bold;
+                    text-align: center;
+                    margin: 0;
+                    padding: 0;
+                }
+
+                h1 {
+                    font-size: 24px; /* Tamanho da tipografia */
+                }
+
+                a {
+                    color: white;
+                    text-decoration: underline;
+                }
+            `}</style>
+        </div>
+    );
 }
 
 export default Home;
+
