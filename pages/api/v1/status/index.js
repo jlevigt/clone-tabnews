6e294ede648e38820015971d93743f0f804aec42
jlevigt <150632922+jlevigt@users.noreply.github.com>
2023-12-13 00:35:51 +0000

    add `/api/v1/status` endpoint

---
+++ pages/api/v1/status/index.js
@@ -0,0 +1,9 @@
+//import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  //const result = await database.query("SELECT 1 + 1;");
+  //console.log(database);
+  response.status(200).json({ chave: "valor" });
+}
+
+export default status;
