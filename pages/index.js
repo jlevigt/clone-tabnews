a0848f7af48fdbf4eac67f48a8c680e1e20c4d9f
jlevigt <150632922+jlevigt@users.noreply.github.com>
2024-03-08 02:49:03 +0000

    alteração na `Home`

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Home() {
+  const [showMessage, setShowMessage] = useState(false);
+
+  const toggleMessage = () => {
+    setShowMessage(!showMessage);
+  };
+
   return (
     <div>
       <title> Site </title>
       <h1>Organização dos PGs</h1>
       <p>Esta é a página 1 do site.</p>
-      <button className="meu-botao">Clique aqui</button>
+      <div className="container">
+        <button className="meu-botao" onClick={toggleMessage}>
+          Clique aqui
+        </button>
+
+        {showMessage && (
+          <div className="message-box">
+            <p>Começando os trabalhos</p>
+          </div>
+        )}
+      </div>
+
       <style jsx global>{`
         body {
           margin: 0;
           padding: 0;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          height: 100vh;
+        }
+
+        .container {
+          text-align: center;
         }
 
         .meu-botao {
-          position: absolute;
-          top: 20px;
-          right: 20px;
           background-color: black;
           color: white;
           border: none;
@@ -25,6 +47,7 @@ function Home() {
           cursor: pointer;
           font-family: inherit;
           font-weight: 600;
+          margin-bottom: 20px;
         }
         .meu-botao:hover {
           background-color: gray;
@@ -32,6 +55,13 @@ function Home() {
 
         h1 {
           font-size: 24px; /* Tamanho da tipografia */
+        }
+
+        .message-box {
+          background-color: #f0f0f0;
+          border: 1px solid #ccc;
+          padding: 10px;
+          margin-top: 10px;
         }
       `}</style>
     </div>
